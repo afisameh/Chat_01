a325654c0d7d9bae7f0fed71282466721eb37b4e
afisameh <afi.sameh87@gmail.com>
2015-08-09 19:10:27 +0200

    Chat Website
    Version  2: 20150809

--- main.php
+++ main.php
@@ -1,24 +1,81 @@
 <?php
 include_once 'dbconfig.php';
-
-$user_id = $_SESSION['user_session'];
-
-$mysqli = new mysqli("localhost", "root", "", "dblogin");
-
-//$stmt = $DB_con->prepare("SELECT * FROM users");
-//$stmt->execute();
-//$userRow = $stmt->fetch(PDO::FETCH_ASSOC);
-
-
-$query = "SELECT * FROM users";
-
-if ($result = $mysqli->query($query)) {
-
-    /* fetch object array */
-    while ($row = $result->fetch_row()) {
-        echo $row[1]+'<br>';
-    }
-
-    $result->close();
+if (!$user->is_loggedin()) {
+    $user->redirect('index.php');
 }
+$user_id = $_SESSION['user_session'];
+$stmt = $DB_con->prepare("SELECT * FROM users WHERE user_id=:user_id");
+$stmt->execute(array(":user_id" => $user_id));
+$userRow = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="facivon.ico">
+
+        <title>Facebook like chat</title>
+        <link href="style.css" rel="stylesheet">
+        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+        <script src="script.js"></script>
+        <title>welcome - <?php print($userRow['user_email']); ?></title>
+    </head>
+    <body>
+        <div class="right">
+            <label><a href="logout.php?logout=true"><i class="glyphicon glyphicon-log-out"></i> logout</a></label>
+        </div>
+        welcome : <?php print($userRow['user_name']); ?>
+        <div class="chat_box">
+            <div class="chat_head"> Chat Box</div>
+            <div class="chat_body"> 
+                <!--get user chat 01-->
+
+                <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "dblogin";
+
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+                $sql = "SELECT * FROM users";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<div class="user" alt="';
+                        echo $row["user_name"];
+                        echo '" id="';
+                        echo $row["user_id"];
+                        echo '">';
+                        echo $row["user_name"];
+                        echo '</div>';
+                    }
+                } else {
+                    echo "0 results";
+                }
+                $conn->close();
+                ?>   
+                <!-- end get user chat 01-->
+            </div>
+        </div>
+        <script>
+            $(document).ready(function () {
+
+                $('.user').click(function () {
+                    var chaine1 = "<div class='msg_box'><div class='msg_head'>";
+                    var chaine2 = $(this).attr("alt");
+                    var chaine3 = "<div class='close'>x</div></div><div class='msg_wrap'><div class='msg_body'><div class='msg_a'><img class='profil_photo' src='images/souhail.jpg' alt='Smiley face'>This is from A</div><div class='msg_b'><img class='profil_photo_b' src='images/images.jpg' alt='Smiley face'>know it even i liked it :)</div><div class='msg_a'><img class='profil_photo' src='images/souhail.jpg' alt='Smiley face'>Wow, Thats great to hear from you man </div><div class='msg_push'></div></div><div class='msg_footer'><textarea class='msg_input' rows='4'></textarea></div></div></div>";
+                    $("#msgs").append(chaine1 + chaine2 + chaine3);
+                });
+            });
+        </script>
+        <div id="msgs"></div>
+    </body>
+</html>
--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	$('.chat_head').click(function(){
-		$('.chat_body').slideToggle('slow');
-	});
-	$('.msg_head').click(function(){
-		$('.msg_wrap').slideToggle('slow');
-	});
-	
-	$('.close').click(function(){
-		$('.msg_box').hide();
-	});
-	
-	$('.user').click(function(){
-
-		$('.msg_wrap').show();
-		$('.msg_box').show();
-	});
-	
-	$('textarea').keypress(
-    function(e){
-        if (e.keyCode == 13) {
-            var msg = $(this).val();
-			$(this).val('');
-			if(msg!='')
-			$('<div class="msg_b">'+msg+'</div>').insertBefore('.msg_push');
-			$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
-        }
+    $('.chat_head').click(function () {
+        $('.chat_body').slideToggle('slow');
     });
-	
+    $('.msg_head').click(function () {
+        $('.msg_wrap').slideToggle('slow');
+    });
+
+    $('.close').click(function () {
+        $('.msg_box').hide();
+    });
+
+    $('.user').click(function () {
+
+//        $('.msg_wrap').show();
+//        $('.msg_box').show();
+//        $("p").append("<strong>Hello</strong>");
+        //      $("p").append("<div class='msg_box' style='right:290px'><div class='msg_head'>Sameh<div class='close'>x</div></div><div class='msg_wrap'><div class='msg_body'><div class='msg_a'>This is from A</div><div class='msg_b'>know it even i liked it :)</div><div class='msg_a'>Wow, Thats great to hear from you man </div><div class='msg_push'></div></div><div class='msg_footer'><textarea class='msg_input' rows='4'></textarea></div></div></div>");});
+
+        $('textarea').keypress(
+                function (e) {
+                    if (e.keyCode == 13) {
+                        var msg = $(this).val();
+                        $(this).val('');
+                        if (msg != '')
+                            $('<div class="msg_b">' + msg + '</div>').insertBefore('.msg_push');
+                        $('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+                    }
+                });
+
+    });
 });
